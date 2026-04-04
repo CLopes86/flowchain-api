e64e2ca1432fa98f9b7987a0fc9c0ed33b8a8ed5
Cesaltino Rodrigues Lopes <c.lopes46cv@gmail.com>
2026-04-04 17:25:52 +0100

    feat: initial project setup with Spring Boot 3.5.13

---
+++ src/main/java/com/flowchain/api/FlowchainApiApplication.java
@@ -0,0 +1,13 @@
+package com.flowchain.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowchainApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowchainApiApplication.class, args);
+	}
+
+}
