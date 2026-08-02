8ef976f29304583f2b68dbbe7dff54410b3f2c20
Cesaltino Rodrigues Lopes <c.lopes46cv@gmail.com>
2026-08-02 09:29:13 +0100

    feat: enable Spring Security and add jjwt dependencies

--- src/main/java/com/flowchain/api/FlowchainApiApplication.java
+++ src/main/java/com/flowchain/api/FlowchainApiApplication.java
@@ -3,9 +3,8 @@ package com.flowchain.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
-})
+@SpringBootApplication
+
 public class FlowchainApiApplication {
 
 	public static void main(String[] args) {
